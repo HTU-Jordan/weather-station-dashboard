4cac4cb8ff9ada26a83f87cdf31bc8079182c63b
Yanal Kashou <yanal.kashou@gmail.com>
2018-01-17 14:07:55 +0200

    Add files via upload

--- init_weatherDB.py
+++ init_weatherDB.py
@@ -1,54 +1,18 @@
 #######################################################################
 # Import Libraries
-import pyodbc
 import pandas as pd
 import numpy as np
+import pyodbc, sqlalchemy
 from sqlalchemy import create_engine
 
 #######################################################################
-# # Define sqlalchemy engine
-# cnxn = pyodbc.connect(r"DRIVER={ODBC Driver 13 for SQL Server};Authentication=ActiveDirectoryIntegrated;TrustServerCertificate=Yes;Encrypt=Yes;DATABASE=weatherDB;WSID=HTU-SQLDEV;APP={Microsoft® Windows® Operating System};SERVER=HTU-SQLDEV"
-# )
-
-# engine = create_engine("pyodbc://@192.168.20.16:1433/?{driver};Trusted_Connection=yes"
-#                       .format(driver="{SQL Server Native Client 11.0}"))
-
-# engine = create_engine('pyodbc://', creator=cnxn, echo=True)
-
-# def connect():
-#     return pyodbc.connect('DSN=weather-ODBC;driver=C:\Windows\system32\odbc32.dll;Database=weatherDB')
-
-# engine = create_engine('sybase+pyodbc://', creator=connect, echo=True)
-#def connect_to_db():
-	#return pyodbc.connect(r'Driver={ODBC Driver 13 for SQL Server};Server=HTU-SQLDEV;Database=weatherDB;Trusted_Connection=yes;')
-
-
-# Open Connection to SQL-Server
-# def connect_to_db():
-# 	cnxn = pyodbc.connect(r"Driver={SQL Server Native Client 11.0};"
-# 						  r"Server=192.168.20.16:1433;"
-# 						  r"Database=weatherDB;"
-# 						  r"Trusted_Connection=yes;")
-# 	cursor = cnxn.cursor()
-# 	return(cursor)
-
-
-import pyodbc, sqlalchemy
-
+# Create connection and engine functions
 def connect():
     return pyodbc.connect('DSN=weather-ODBC;Trusted_Connection=yes')
 
 def create_eng():
 	engine = sqlalchemy.create_engine('mssql+pyodbc://', creator=connect, echo=True)
 	return(engine)
-#######################################################################
-# Get Record from DB
-# def get_data():
-#     cursor = connect_to_db()
-#     record = cursor.execute("SELECT * from perFIFTEENSEC").fetchall()
-#     return(record[-1])
-    
-
 
 #######################################################################
 # Read data from file and get last record
@@ -81,7 +45,6 @@ def append_to_db_perMINUTE(x):
 # MAIN LOOP
 #######################################################################
 if __name__ == '__main__':
-	#connect_to_db()
 	connect()
 	engine = create_eng()
 	df_minute, df_hour = read_data()
